e9ce1d7bd0557450c13268f960247e8ad0f03326
MGS <mo.gholipoor1@gmail.com>
2024-12-14 18:04:01 +0330

    modify bisection with sympy

--- bisection.py
+++ bisection.py
@@ -1,3 +1,4 @@
+from sympy import *
 def print_box(content):
     content_str = f"║ {content} ║"
     width = len(content_str)
@@ -14,14 +15,8 @@ def print_brace(title, a, b):
     print(f" b = {b}".ljust(width + 2))
     print(" " + " " * (width // 2) + "⎩")
 
-# f(x) = x^2
-def TargetFuntion(num):
-    return num ** 2
 
-def function_prime(num):
-    return 2*num
-
-def bisection(a,b,ebsilon,iteration):
+def bisection(a,b,ebsilon,iteration,F,X,FPrime):
     print("▀" * 50)    
     print_box(f"iteration = {iteration}")
     iteration += 1
@@ -30,19 +25,19 @@ def bisection(a,b,ebsilon,iteration):
         x = (a+b)/2
         print(f"a == {a}\nb == {b}\nx == {x}")
         
-        fPrimeOfX = function_prime(x)
+        fPrimeOfX = FPrime.subs(X,x)
         # print(f"fPrimeOfX: {fPrimeOfX}")
 
         if fPrimeOfX < 0:
             print(f"**********************\nf'(x) < 0\n**********************\na = {x}\nb = {b}")
             a = x
             b = b
-            return bisection(a,b,ebsilon,iteration)
+            return bisection(a,b,ebsilon,iteration,F,X,FPrime)
         elif fPrimeOfX > 0:
             print(f"**********************\nf'(x)> 0\n**********************\na = {a}\nb = {x}")
             a = a
             b = x
-            return bisection(a,b,ebsilon,iteration)
+            return bisection(a,b,ebsilon,iteration,F,X,FPrime)
         else:
             print("**********************\nf'(x) = 0\n**********************")
             return x
@@ -50,4 +45,12 @@ def bisection(a,b,ebsilon,iteration):
 
         return (a+b)/2
 
-print(f"result: {bisection(a=-11,b=8,ebsilon=0.0000001,iteration=0)}")
+
+
+X = symbols('X')
+F = X**2
+FPrime = diff(F,X)
+
+print(f"result: {bisection(a=-11,b=8,ebsilon=0.0000001,iteration=0,F=F,X=X,FPrime=FPrime)}")
+print(f"f = {F}")
+print(f"f' = {FPrime}")
